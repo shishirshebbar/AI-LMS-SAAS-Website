df4e0cd985c65d1d91c5a55b3391d99c1f3b7bc7
Shish <shishir4393@gmail.com>
2025-01-13 00:19:48 +0530

    Database setup

--- utils/dbschema.js
+++ utils/dbschema.js
@@ -1,8 +1,22 @@
-import { boolean, pgTable, serial, varchar} from "drizzle-orm/pg-core";
+
+import { boolean, json, pgTable, serial, varchar} from "drizzle-orm/pg-core";
 
 export const USER = pgTable('users',{
     id:serial().primaryKey(),
     name:varchar().notNull(),
     email:varchar().notNull(),
     isMember:boolean().default(false)
+})
+
+export const STUDY_MATERIAL = pgTable('studyMaterial',{
+    id:serial().primaryKey(),
+    courseId:varchar().notNull(),
+    courseType:varchar().notNull(),
+    topic:varchar().notNull(),
+    difficultyLevel:varchar().default('Easy'),
+    courseLayout:json(),
+    createdBy:varchar().notNull(),
+    status:varchar().default('Generating')
+
+
 })
